fe510e15dae7bab806cf58af93fc86878d2c4694
Ваше Имя <you@example.com>
2019-10-16 17:53:45 +0300

    Кнопка кто ее нажал

--- src/server.js
+++ src/server.js
@@ -5,38 +5,44 @@ const url = require('url');
 //telegram-bot-api
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
-const token = '725276890:AAEDGzGuSb8btbRGXGMPa8fvskt2QSasiIw';
+const token = '725276890:AAFZsqsDgLvLfhgY8t-9lhjhCN-ZwAazqUM';
 
 //PROXY
 const bot = new TelegramBot(token, {
   polling: true,
   request: {
-    proxy:"http://46.105.57.149:43878"
-  }});
-
-//Index.html делаем страницей по умолчанию
-// app.get('/', function (req, res) {
-//   res.sendfile(__dirname + "/index.html");
-// });
-
-app.get('/site?/', function(req, res){
-
-  let Url = req.url;
-  let parseUrl = url.parse(Url,true,true);
-
-  let str = '';
-  let tel_str = '';
-
-  for (var key in parseUrl.query){
-    str += ("Ключ: " + key + " Значение: " + parseUrl.query[key]+ "<br>");
-    tel_str += ("Ключ: " + key + " Значение: " + parseUrl.query[key]+ "\n");
+    proxy: "http://116.203.127.92:3128"
   }
-
-  bot.sendMessage('311805730', tel_str );
-  res.send(str);
 });
 
-// запускаем новый HTTP-сервер на порту 3000
-app.listen(3000, function () {
-  console.log('Сервер работает на порту : 3000!');
+var questions = [{
+  title: 'Кто нажал на эту кнопку?',
+  buttons: [
+    [{
+      text: 'click me',
+      callback_data: '1'
+    }]
+  ]
+}];
+
+function newQuestion(msg) {
+  var arr = questions[0];
+  var text = arr.title;
+  var options = {
+    reply_markup: JSON.stringify({
+      inline_keyboard: arr.buttons
+    })
+  };
+  chat = msg.hasOwnProperty('chat') ? msg.chat.id : msg.from.id;
+  console.log(options)
+  bot.sendMessage(chat, text, options);
+
+}
+
+bot.onText(/\/start/, function(msg, match) {
+  newQuestion(msg);
+});
+
+bot.on('callback_query', function(msg) {
+  bot.sendMessage(msg.from.id, msg.from.first_name + ' ' + msg.from.last_name);
 });
