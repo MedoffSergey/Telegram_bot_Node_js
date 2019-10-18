bf7ce1a39729fcaee9cdadd0a0f42112753f1fe0
Ваше Имя <you@example.com>
2019-10-18 16:42:09 +0300

    rating\ ставим лайки в чате телеграм

--- src/server.js
+++ src/server.js
@@ -1,8 +1,3 @@
-const express = require('express');
-const app = express();
-const url = require('url');
-
-//telegram-bot-api
 const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 const token = '725276890:AAFZsqsDgLvLfhgY8t-9lhjhCN-ZwAazqUM';
@@ -11,38 +6,66 @@ const token = '725276890:AAFZsqsDgLvLfhgY8t-9lhjhCN-ZwAazqUM';
 const bot = new TelegramBot(token, {
   polling: true,
   request: {
-    proxy: "http://116.203.127.92:3128"
+    proxy: "http://194.26.180.142:3128"
   }
 });
 
-var questions = [{
-  title: 'Кто нажал на эту кнопку?',
-  buttons: [
-    [{
-      text: 'click me',
-      callback_data: '1'
-    }]
-  ]
-}];
-
-
 function newQuestion() {
-  var arr = questions[0];
-  var text = arr.title;
-  var options = {
-    reply_markup: JSON.stringify({
-      inline_keyboard: arr.buttons
-    })
-  };
-  bot.sendMessage(311805730, text, options);
+  bot.sendMessage(-276583637, 'Работяга john вышел на работу');
 }
-
 newQuestion();
 
-bot.on('callback_query', msg => {
-    bot.editMessageText(msg.message.text+' '+ msg.from.first_name + ' ' + msg.from.last_name,  {
-                chat_id: msg.from.id,
-                message_id: msg.message.message_id
-            })
+let like = [];
+let dislike = []
 
+bot.onText(/.+/, function(msg, match) {
+  if (msg.reply_to_message && msg.from.id != msg.reply_to_message.from.id && match[0] === "+" ||
+    match[0].toLowerCase() === "спасибо" || match[0].toLowerCase() === "спс" || match[0] === "👍") {
+    like.push(msg.reply_to_message.from.first_name)
+  }
+  else if (msg.reply_to_message && msg.from.id != msg.reply_to_message.from.id && match[0] === "-" ||
+    match[0].toLowerCase() === "дизлайк" || match[0].toLowerCase() === "нет" || match[0] === "👎") {
+    dislike.push(msg.reply_to_message.from.first_name)
+  }
+})
+
+function countPlus(like, dislike) {
+  let finalRating = {};
+
+  for (let i = 0; i < like.length; i++) {
+    if (finalRating[like[i]] === undefined) {
+      finalRating[like[i]] = 1;
+    } else {
+      finalRating[like[i]]++;
+    }
+  };
+  for (let i = 0; i < dislike.length; i++) {
+    if (finalRating[dislike[i]] === undefined) {
+      finalRating[dislike[i]] = -1;
+    } else {
+      finalRating[dislike[i]]--;
+    }
+  }
+  console.log(finalRating)
+  return finalRating
+}
+
+
+bot.onText(/\/rating/, function showRating(msg) {
+
+  let object = countPlus(like, dislike)
+  let result = [];
+  for (let name in object) {
+    result.push([name, object[name]]);
+  }
+  result.sort(function(a, b) {
+    return b[1] - a[1];
+  });
+
+  let message = ""
+  for (let i = 0; i < result.length; i++) {
+    message += i + 1 + ' место ' + result[i][0] + " : " + result[i][1] + " 💙 \n";
+  }
+
+  bot.sendMessage(msg.chat.id, message)
 })
